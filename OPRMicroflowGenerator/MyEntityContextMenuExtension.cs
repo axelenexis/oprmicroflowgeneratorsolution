6a6dc731e8685a2b504e56954f6eea01bd24178c
Axel Brink <axel.brink@enexis.nl>
2026-07-03 09:19:20 +0200

    Initial upload.

---
+++ OPRMicroflowGenerator/MyEntityContextMenuExtension.cs
@@ -0,0 +1,24 @@
+﻿using Mendix.StudioPro.ExtensionsAPI.Model.DomainModels;
+using Mendix.StudioPro.ExtensionsAPI.UI.Menu;
+using System.ComponentModel.Composition;
+ 
+namespace OPRMicroflowGenerator;
+
+[method: ImportingConstructor]
+[Export(typeof(ContextMenuExtension<>))]
+class MyEntityContextMenuExtension(OPRMicroflowGenerator microflowGenerator) : ContextMenuExtension<IEntity>
+{
+    public override IEnumerable<MenuViewModel> GetContextMenus(IEntity entity)
+    {
+        yield return new MenuViewModel("Generate OPR microflows", () =>
+        {
+            if (CurrentApp == null)
+                return;
+
+            microflowGenerator.GenerateOPRMicroflows(CurrentApp, entity);
+        });
+
+    }
+
+
+}
---
+++ OPRMicroflowGenerator/OPRMicroflowGenerator.cs
@@ -0,0 +1,64 @@
+﻿using Mendix.StudioPro.ExtensionsAPI.Model;
+using Mendix.StudioPro.ExtensionsAPI.Model.DomainModels;
+using Mendix.StudioPro.ExtensionsAPI.Model.Enumerations;
+using Mendix.StudioPro.ExtensionsAPI.Model.MicroflowExpressions;
+using Mendix.StudioPro.ExtensionsAPI.Model.Microflows;
+using Mendix.StudioPro.ExtensionsAPI.Model.DataTypes;
+using Mendix.StudioPro.ExtensionsAPI.Services;
+using System.ComponentModel.Composition;
+
+namespace OPRMicroflowGenerator;
+
+[Export(typeof(OPRMicroflowGenerator))]
+[method: ImportingConstructor]
+class OPRMicroflowGenerator(IMicroflowService microflowService, IMicroflowExpressionService microflowExpressionService)
+{
+    public void GenerateOPRMicroflows(IModel currentApp, IEntity entity)
+    {
+        //var currentApp = CurrentApp;
+        string moduleName = entity.QualifiedName.FullName.Split('.')[0];
+        var module = currentApp.Root.GetModules().Single(m => m.Name == moduleName);
+
+        using var transaction = currentApp!.StartTransaction("Generate OPR microflows for entity " + entity.Name);
+
+        var createMicroflow = microflowService.CreateMicroflow(currentApp, module, "OPR_" + entity.Name + "_Create");
+        foreach (IAttribute attribute in entity.GetAttributes())
+        {
+            MicroflowReturnValue? returnValue = null;
+            IMicroflowExpression returnExpression = microflowExpressionService.CreateFromString($"${entity.Name}/{attribute.Name}");
+
+            if (attribute.Type is IStringAttributeType)
+            {
+                returnValue = new MicroflowReturnValue(DataType.String, returnExpression);
+            }
+            else if (attribute.Type is IIntegerAttributeType)
+            {
+                returnValue = new MicroflowReturnValue(DataType.Integer, returnExpression);
+            }
+            else if (attribute.Type is IBooleanAttributeType)
+            {
+                returnValue = new MicroflowReturnValue(DataType.Boolean, returnExpression);
+            }
+            else if (attribute.Type is IDecimalAttributeType)
+            {
+                returnValue = new MicroflowReturnValue(DataType.Decimal, returnExpression);
+            }
+            else if (attribute.Type is IDateTimeAttributeType)
+            {
+                returnValue = new MicroflowReturnValue(DataType.DateTime, returnExpression);
+            }
+            else if (attribute.Type is IEnumerationAttributeType)
+            {
+                IQualifiedName<IEnumeration> enumQualifiedName = ((IEnumerationAttributeType)attribute.Type).Enumeration;
+                returnValue = new MicroflowReturnValue(DataType.Enumeration(enumQualifiedName), returnExpression);
+            }
+            var inputParameter = (entity.Name, DataType.Object(entity.QualifiedName));
+            var getMicroflow = microflowService.CreateMicroflow(currentApp, module, "OPR_" + entity.Name + "_Get" + attribute.Name,
+                returnValue, inputParameter);
+            var setMicroflow = microflowService.CreateMicroflow(currentApp, module, "OPR_" + entity.Name + "_Set" + attribute.Name, null, inputParameter);
+        }
+
+        transaction.Commit();
+    }
+}
+
